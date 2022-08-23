8841ba8c3450faf2383d4a3ea75dd861f586ddf5
Shimoni Sandeep <shimoniharidas@gmail.com>
2022-08-23 13:09:51 +0930

    Updated

--- src/introduction/nestedFrames.java
+++ src/introduction/nestedFrames.java
@@ -5,13 +5,21 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class nestedFrames {
 
+	
 	public static void main(String[] args) {
 
 		// TODO Auto-generated method stub
 
+		getFrames();
+
+	}
+
+	@Test
+	private static void getFrames() {
 		System.setProperty("webdriver.chrome.driver", "C:\\Softwares\\chromedriver_win32\\chromedriver.exe");
 
 		WebDriver driver = new ChromeDriver();
@@ -39,7 +47,7 @@ public class nestedFrames {
 		  driver.switchTo().parentFrame();
 		  driver.switchTo().frame("frame-left");
 		  System.out.println(driver.findElement(By.xpath("//body")).getText());
-
+		  driver.close();
 	}
 
 }
